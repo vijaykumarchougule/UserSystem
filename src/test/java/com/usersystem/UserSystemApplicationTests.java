bca05ab2eead7b43c50b00adf59dc2c24b41cbda
Vijaykumar <vijaykumarchougule2001@gmail.com>
2023-04-21 00:51:04 +0530

    Start Project -VJ

---
+++ src/test/java/com/usersystem/UserSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
